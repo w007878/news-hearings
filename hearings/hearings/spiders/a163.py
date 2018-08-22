2459877781e1c516ab036b4e6c4ad97ecdb6063d
snowyjone <w007878@hotmail.com>
2018-08-23 07:21:00 +0800

    little change

--- hearings/hearings/spiders/a163.py
+++ hearings/hearings/spiders/a163.py
@@ -5,7 +5,10 @@ import scrapy
 class A163Spider(scrapy.Spider):
     name = '163'
     allowed_domains = ['163.com']
-    start_urls = ['http://163.com/']
+    start_urls = ['https://news.163.com/']
 
+    queue = []
     def parse(self, response):
+        print(response.xpath('//a'))
+        print('QAQ')
         pass
